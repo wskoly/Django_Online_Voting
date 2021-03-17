659d7c804bf09360ca74759f1227a60da5650563
Wahid Sadique Koly <wskoly.bp@gmail.com>
2021-03-17 21:10:48 +0600

    updated all other urls

--- online_vote/urls.py
+++ online_vote/urls.py
@@ -18,10 +18,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from muni_election import views
+from django.contrib.auth.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='index'),
     path('vote/', views.vote, name='vote'),
     path('standings/', views.standings, name='standings'),
+    path('logout/', views.voter_logout, name='logout'),
+    path('password_change/', PasswordChangeView.as_view(success_url ='done', template_name= 'muni_election/change_pw.html'), name='change_password'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name= 'muni_election/pw_change_done.html'), name='change_password_done'),
+    path('password_reset/', PasswordResetView.as_view(html_email_template_name = 'muni_election/pw_reset_email.html',subject_template_name ='muni_election/pw_reset_subject.txt'), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('voter_reg', views.voter_reg, name='voter_reg'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
