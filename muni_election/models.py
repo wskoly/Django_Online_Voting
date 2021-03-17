e7e29f03c1b3130d2ff2dc8fb46149d487225b74
Wahid Sadique Koly <wskoly.bp@gmail.com>
2021-03-17 18:40:56 +0600

    Updated models.py

--- muni_election/models.py
+++ muni_election/models.py
@@ -7,42 +7,59 @@ class user(AbstractUser):
 
 #relational model voter area
 class voter_area(models.Model):
-    area_code = models.CharField(max_length=200, primary_key=True)
-    area_name = models.CharField(max_length=200)
-    area_subDist = models.CharField(max_length=200)
-    area_dist = models.CharField(max_length=200)
-    area_div = models.CharField(max_length=200)
+    code = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
+    subDist = models.CharField(max_length=200)
+    dist = models.CharField(max_length=200)
+    div = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name +"(Code:"+ self.code+")"
 
 #voter model which also has OneToOne relation with user model
 class voter(models.Model):
-    voter_user = models.OneToOneField(user, on_delete=models.CASCADE)
+    gen_choice = (('Male','Male'),('Female','Female'),('Others','Others'),)
+
+    user = models.OneToOneField(user, on_delete=models.CASCADE)
     voter_id = models.CharField(max_length=17, primary_key=True, unique=True)
-    voter_area = models.ForeignKey(voter_area, on_delete=models.CASCADE)
-    voter_serial = models.IntegerField()
-    voter_dob = models.DateField()
-    voter_gender = models.CharField(max_length=20)
-    voter_ward = models.IntegerField()
-    municipality = models.CharField
+    area = models.ForeignKey(voter_area, on_delete=models.CASCADE)
+    serial = models.IntegerField()
+    dob = models.DateField()
+    gender = models.CharField(max_length=20,choices=gen_choice)
+    ward = models.IntegerField()
+    municipality = models.CharField(max_length=50)
+    def __str__(self):
+        return self.voter_id
+
 #staff model
 class election_staff(models.Model):
-    staff_user = models.OneToOneField(user, on_delete=models.CASCADE)
-    staff_role = models.CharField(max_length=200)
+    role_choice = (('Data Migrator','Data Migrator'),('Election Manager','Election Manager'))
 
+    user = models.OneToOneField(user, on_delete=models.CASCADE)
+    role = models.CharField(max_length=200, choices=role_choice)
+    def __str__(self):
+        return self.user
+
+#election model
 class election(models.Model):
-    election_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     num_of_ward = models.IntegerField()
     is_open = models.BooleanField(default=False)
     is_res_published = models.BooleanField(default=False)
+    election_areas = models.ManyToManyField(voter_area)
 
-class election_areas(models.Model):
-    election_id = models.ForeignKey(election, on_delete=models.CASCADE)
-    voter_area = models.ForeignKey(voter_area, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.election_name
 
 class mayor_candidate(models.Model):
     election_id = models.ForeignKey(election, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    picture = models.ImageField(upload_to = 'candidate_pic/')
     symbol = models.ImageField(upload_to='elec_symbol/')
     vote_count = models.IntegerField()
+
+    def __str__(self):
+        return self.name+"(election_id:"+ self.election_id+")"
 
 class m_councilor_candidate(models.Model):
     election_id = models.ForeignKey(election, on_delete=models.CASCADE)
@@ -50,6 +67,8 @@ class m_councilor_candidate(models.Model):
     symbol = models.ImageField(upload_to='elec_symbol/')
     ward_no = models.IntegerField()
     vote_count = models.IntegerField()
+    def __str__(self):
+        return self.name+"(election_id:"+ self.election_id+")"
 
 class fe_councilor_candidate(models.Model):
     election_id = models.ForeignKey(election, on_delete=models.CASCADE)
@@ -58,6 +77,8 @@ class fe_councilor_candidate(models.Model):
     start_ward = models.IntegerField()
     end_ward = models.IntegerField()
     vote_count = models.IntegerField()
+    def __str__(self):
+        return self.name+"(election_id:"+ self.election_id+")"
 
 
 
