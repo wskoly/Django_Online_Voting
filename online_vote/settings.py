9595a9085dda9f868b3c433349216dc42aeccfdf
Wahid Sadique Koly <wskoly.bp@gmail.com>
2021-03-30 21:10:47 +0600

    Update settings.py

--- online_vote/settings.py
+++ online_vote/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR.joinpath('templates')
@@ -113,6 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -148,3 +148,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER ='online.election.django@gmail.com'
 EMAIL_HOST_PASSWORD = Smtp_pw
 EMAIL_PORT = 587
+
+
