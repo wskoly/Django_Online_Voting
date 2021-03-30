b96938b9e51b9dde8ad36224439720ae36c48f83
Wahid Sadique Koly <wskoly.bp@gmail.com>
2021-03-30 21:10:40 +0600

    Update views.py

--- muni_election/views.py
+++ muni_election/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render,redirect
-from .forms import VoterLoginForm,userReg,voterReg
+from .forms import *
 from django.contrib.auth import authenticate, login, logout,get_user_model
 from django.contrib.auth.decorators import login_required
-from muni_election.models import *
 from django.forms.models import model_to_dict as mTd
-from django.db.models import Q
-from django.utils.translation import gettext, gettext_lazy as _
+from django.utils.translation import gettext, gettext_lazy as _, get_language,activate
 from django.contrib.auth.hashers import make_password as voter_hasher
 from django.core.exceptions import ObjectDoesNotExist
+from  .utilities import *
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import messages
 
 #index view
 def index(request):
@@ -35,12 +36,15 @@ def vote(request):
         vot = voter.objects.get(user = user)
         area = vot.area.code
         ward = vot.ward
+        request.session['ward'] = ward
         try:
             election_id = election.election_areas.through.objects.get(voter_area_id=area).election_id
         except ObjectDoesNotExist:
             election_id = None
         if election_id:
             electionObj = election.objects.get(pk=election_id)
+            request.session['election_id'] = election_id
+            #request.session['electionObj'] = electionObj
             IS_VOTED = is_voted.objects.filter(Q(election_id=electionObj)&Q(user=user)).exists()
             if not IS_VOTED:
                 if request.method == 'POST':
@@ -55,11 +59,11 @@ def vote(request):
                         re_councilor_vote = request.POST['re_councilor']
                         re_councilorObj = re_councilor_candidate.objects.get(pk=re_councilor_vote)
                         votes = vote_store(voter_hash=voter_hash, election_id=electionObj, mayor_candidate=mayorObj, councilor_candidate = councilorObj, re_councilor_candidate=re_councilorObj)
-                        voted = is_voted(election_id=electionObj, user = user)
+                        voted = is_voted(election_id=electionObj, user = user, ward=ward)
                         votes.save()
                         voted.save()
+                        calculate_vote(election_id)
                         return redirect('vote_done')
-                        print(voter_hash,voted,votes)
                     except Exception as e:
                         print(e)
                     print(request.POST['mayor'])
@@ -92,7 +96,34 @@ def vote_done(request):
 
 @login_required(login_url='index')
 def standings(request):
-    return render(request,'muni_election/standings.html')
+    if request.user.is_voter:
+        wc = "Welcome " + request.user.first_name + " " + request.user.last_name
+        election_id = request.session.get('election_id')
+        ward = request.session.get('ward')
+        if election_id:
+            #ward list of reserve councilor
+            re_c_Obj = re_councilor_candidate.objects.filter(Q(election_id=election_id)&(Q(reserve_ward_1=ward)|Q(reserve_ward_2=ward)|Q(reserve_ward_3=ward))).first()
+            re_ward1 = re_c_Obj.reserve_ward_1
+            re_ward2 =re_c_Obj.reserve_ward_2
+            re_ward3 = re_c_Obj.reserve_ward_3
+
+            #Total Vote casted for mayor on that particular election
+            mayor_total = is_voted.objects.filter(election_id=election_id).count()
+
+            # Total Vote casted for councilor on that particular election and that particular ward
+            councilor_total = is_voted.objects.filter(Q(election_id=election_id)&Q(ward=ward)).count()
+
+            # Total Vote casted for councilor on that particular election and that particular ward
+            re_councilor_total = is_voted.objects.filter(Q(election_id=election_id)&(Q(ward=re_ward1)|Q(ward=re_ward2)|Q(ward=re_ward3))).count()
+            mayors = mayor_candidate.objects.filter(election_id=election_id)
+            councilors = councilor_candidate.objects.filter(Q(election_id=election_id) & Q(ward_no=ward))
+            re_councilors = re_councilor_candidate.objects.filter(Q(election_id=election_id) & (Q(reserve_ward_1=ward) | Q(reserve_ward_2=ward) | Q(reserve_ward_3=ward)))
+            context = {'wc':wc, 'has_election': True, 'mayors':mayors, 'councilors':councilors, 're_councilors':re_councilors, 'mayor_total':mayor_total, 'councilor_total':councilor_total, 're_councilor_total':re_councilor_total}
+        else:
+            context = {'wc': wc, 'has_election': False}
+    else:
+        return redirect('index')
+    return render(request,'muni_election/standings.html', context=context)
 
 @login_required(login_url='index')
 def voter_logout(request):
@@ -121,3 +152,16 @@ def voter_reg(request):
 
 def voter_reg_complete(request):
     return render(request,'main/voter_reg_complete.html')
+
+@staff_member_required
+def voter_migrate(request):
+    if request.method == 'POST':
+        form = migrateVoter(request.POST)
+        if form.is_valid():
+            voter_migration_func(str(request.POST['start_date']),str(request.POST['end_date']))
+            messages.success(request, "Migration Done Successfully")
+    else:
+        form = migrateVoter()
+    context = {'has_permission':True,'site_header':'Online Election Platform Administration', 'form':form, 'title': 'Migrate Voters From National DB' }
+    return render(request,'muni_election/migrate_voter.html',context)
+
