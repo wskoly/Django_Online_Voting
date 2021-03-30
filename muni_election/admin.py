8646787d47c9232bd3f0ba469d43ac61dc23704c
Wahid Sadique Koly <wskoly.bp@gmail.com>
2021-03-30 21:09:58 +0600

    Update admin.py

--- muni_election/admin.py
+++ muni_election/admin.py
@@ -2,7 +2,8 @@ from django.utils.translation import gettext, gettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from muni_election.models import *
-
+from django.urls import path
+from muni_election.views import voter_migrate
 
 class adminUser(BaseUserAdmin):
     fieldsets = (
@@ -13,30 +14,59 @@ class adminUser(BaseUserAdmin):
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
+class areaAdmin(admin.ModelAdmin):
+    list_display = ['code','name','subDist','dist','div']
+
+class voterAdmin(admin.ModelAdmin):
+    list_display = ['user', 'voter_id','get_name','get_email','get_phone', 'area', 'serial', 'dob', 'gender', 'ward', 'municipality']
+
+class electionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'num_of_ward', 'area_names', 'is_open', 'is_res_published']
 
 class MayorAdmin(admin.ModelAdmin):
     exclude = ('vote_count',)
+    list_display = ['election_id','name', 'vote_count']
 
 class CouncilorAdmin(admin.ModelAdmin):
     exclude = ('vote_count',)
+    list_display = ['election_id', 'name', 'vote_count']
 
 class ReCouncilorAdmin(admin.ModelAdmin):
     exclude = ('vote_count',)
+    list_display = ['election_id', 'name', 'vote_count']
 
 class vote_st(admin.ModelAdmin):
     readonly_fields = ['voter_hash','election_id', 'mayor_candidate', 'councilor_candidate', 're_councilor_candidate']
+
+class staffAdmin(admin.ModelAdmin):
+    list_display = ['user','staff_name','role']
+
 class voted(admin.ModelAdmin):
     readonly_fields = ['election_id','user']
+
+class voterMigrateAdmin(admin.ModelAdmin):
+    model = voter_migration
+
+    def get_urls(self):
+        view_name = '{}_{}_changelist'.format(
+            self.model._meta.app_label, self.model._meta.model_name)
+        return [
+            path('', voter_migrate, name=view_name),
+        ]
+
+
+admin.site.register(voter_migration, voterMigrateAdmin)
+
 #change admin panel
 admin.site.site_header = "Online Voting Administration"
 admin.site.site_title = "Online Voting Admin"
 admin.site.index_title = "Online Voting Administration"
 # Register your models here.
 admin.site.register(user, adminUser)
-admin.site.register(voter)
-admin.site.register(voter_area)
-admin.site.register(election_staff)
-admin.site.register(election)
+admin.site.register(voter,voterAdmin)
+admin.site.register(voter_area,areaAdmin)
+admin.site.register(election_staff,staffAdmin)
+admin.site.register(election,electionAdmin)
 admin.site.register(mayor_candidate, MayorAdmin)
 admin.site.register(councilor_candidate,CouncilorAdmin)
 admin.site.register(re_councilor_candidate,ReCouncilorAdmin)
